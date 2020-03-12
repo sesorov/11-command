b303b4661f8562601658d3cbc2fa086292f5dd67
trifonovDmitry <48796062+trifonovDmitry@users.noreply.github.com>
2020-03-12 10:44:32 +0300

    Add files via upload

--- chat_bot_template.py
+++ chat_bot_template.py
@@ -3,7 +3,8 @@
 
 import logging
 import time
-
+import json
+import os
 from setup import PROXY, TOKEN
 from telegram import Bot, Update
 from telegram.ext import CallbackContext, CommandHandler, Filters, MessageHandler, Updater
@@ -18,6 +19,7 @@ logger = logging.getLogger(__name__)
 # context. Error handlers also receive the raised TelegramError object in error.
 
 users_action = []
+USER_ACTIONS_FILE = "user_actions.json"
 
 
 def command_handle(func):
@@ -28,8 +30,9 @@ def command_handle(func):
                 'user_name': update.effective_user.first_name,
                 'function': func.__name__,
                 'text': update.message.text,
-                'time': time.ctime()
+                'time': time.strftime("%H:%M:%S", time.localtime())
             })
+        save_history()
         return func(*args, **kwargs)
     return inner
 
@@ -46,19 +49,34 @@ def chat_help(update: Update, context: CallbackContext):
     update.message.reply_text('Введи команду /start для начала. ')
 
 
-@command_handle
-def history(update: Update, context: CallbackContext):
-    with open("users_actions.txt", 'w+') as text_action:
-        for i in users_action[len(users_action):len(users_action) - 6:-1]:
-            update.message.reply_text(i)
-            text_action.write(str(i) + '\n')
-    text_action.close()
+def get_history(update: Update, context: CallbackContext):
+    count = 0
+    for action in users_action[::-1]:
+        if action["user_name"] == update.effective_user.first_name and count != 5:
+            update.message.reply_text(f"Function: {action['function']},"
+                                      f" text: \"{action['text']}\", time: {action['time']}")
+            count += 1
+    if count == 0:
+        update.message.reply_text("You have't done anything yet")
 
 
 @command_handle
 def echo(update: Update, context: CallbackContext):
     """Echo the user message."""
     update.message.reply_text(update.message.text)
+
+
+def save_history():
+    with open(USER_ACTIONS_FILE, mode="w", encoding="utf-8") as file:
+        json.dump(users_action, file, ensure_ascii=False, indent=2)
+
+
+def load_history():
+    global users_action
+    if os.stat(USER_ACTIONS_FILE).st_size == 0:
+        return
+    with open(USER_ACTIONS_FILE, mode="r", encoding="utf-8") as file:
+        users_action = json.load(file)
 
 
 def error(update: Update, context: CallbackContext):
@@ -71,11 +89,12 @@ def main():
         token=TOKEN,
         base_url=PROXY,  # delete it if connection via VPN
     )
+    load_history()
     updater = Updater(bot=bot, use_context=True)
     # on different commands - answer in Telegram
     updater.dispatcher.add_handler(CommandHandler('start', start))
     updater.dispatcher.add_handler(CommandHandler('help', chat_help))
-    updater.dispatcher.add_handler(CommandHandler('history', history))
+    updater.dispatcher.add_handler(CommandHandler('history', get_history))
 
     # on noncommand i.e message - echo the message on Telegram
     updater.dispatcher.add_handler(MessageHandler(Filters.text, echo))
